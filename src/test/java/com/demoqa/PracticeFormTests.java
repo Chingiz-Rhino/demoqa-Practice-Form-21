86c2d00bdb9f4e110ae41033eb120264957f83f8
Chingiz <chingiz-askarov@list.ru>
2023-07-19 23:50:16 +0300

    added build.gradle

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -1,6 +1,7 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -17,12 +18,16 @@ public class PracticeFormTests {
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy="eager";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1980x1200";
     }
     @Test
     void FillFormTest(){
+
+
         open("/automation-practice-form");
 
+        Selenide.executeJavaScript("$('#fixedban').remove()");
+        Selenide.executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue("Askarov");
         $("#lastName").setValue("Chingiz");
@@ -33,9 +38,9 @@ public class PracticeFormTests {
         $(".react-datepicker__month-select").$(byText("November")).click();
         $(".react-datepicker__year-select").$(byText("1995")).click();
         $("div.react-datepicker__day--020").click();
-        $("#subjectsInput").setValue("Mother Russia");
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#currentAddress").setValue("Moscow");
+        $("#currentAddress").setValue("Haryana Karnal");
         $("#uploadPicture").uploadFile(new File("src/test/resources/flag.jpg"));
         $("#stateCity-wrapper").$(byText("Select State")).click();
         $("#react-select-3-option-2").click();
@@ -48,12 +53,13 @@ public class PracticeFormTests {
         $("tbody tr:nth-child(2) td:nth-child(2)").shouldHave(text("chingiz-askarov@list.ru"));
         $("tbody tr:nth-child(3) td:nth-child(2)").shouldHave(text("Male"));
         $("tbody tr:nth-child(4) td:nth-child(2)").shouldHave(text("9650490205"));
-        $("tbody tr:nth-child(5) td:nth-child(2)").shouldHave(text("20 Novemder, 1995"));
-        $("tbody tr:nth-child(6) td:nth-child(2)").shouldHave(text("Mother Russia"));
+        $("tbody tr:nth-child(5) td:nth-child(2)").shouldHave(text("20 November,1995"));
+        $("tbody tr:nth-child(6) td:nth-child(2)").shouldHave(text("Computer Science"));
         $("tbody tr:nth-child(7) td:nth-child(2)").shouldHave(text("Sports"));
         $("tbody tr:nth-child(8) td:nth-child(2)").shouldHave(text("flag.jpg"));
         $("tbody tr:nth-child(9) td:nth-child(2)").shouldHave(text("Haryana Karnal"));
 
 
     }
+
 }
