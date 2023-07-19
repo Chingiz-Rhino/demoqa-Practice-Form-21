d8e62a69ae6582308120db6b1384d1202bb7ff13
Chingiz <chingiz-askarov@list.ru>
2023-07-19 23:23:50 +0300

    added build.gradle

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -15,6 +17,7 @@ public class PracticeFormTests {
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy="eager";
+        Configuration.browserSize = "1920x1080";
     }
     @Test
     void FillFormTest(){
@@ -23,19 +26,34 @@ public class PracticeFormTests {
 
         $("#firstName").setValue("Askarov");
         $("#lastName").setValue("Chingiz");
-       // $("#userEmail").setValue("chingiz-askarov@list.ru");
+        $("#userEmail").setValue("chingiz-askarov@list.ru");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("9650490205");
-        //$("#dateOfBirthInput").setValue("20 Nov 1995");
-       // $("#subjectsInput").setValue("Mother Russia");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("November")).click();
+        $(".react-datepicker__year-select").$(byText("1995")).click();
+        $("div.react-datepicker__day--020").click();
+        $("#subjectsInput").setValue("Mother Russia");
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#currentAddress").setValue("Moscow");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/flag.jpg"));
+        $("#stateCity-wrapper").$(byText("Select State")).click();
+        $("#react-select-3-option-2").click();
+        $("#stateCity-wrapper").$(byText("Select City")).click();
+        $("#react-select-4-option-0").click();
+
         $("#submit").click();
 
-        $(".modal-body").shouldHave(text("Askarov"));
-        $(".modal-body").shouldHave(text("Chingiz"));
-        $(".modal-body").shouldHave(text("chingiz-askarov@list.ru"));
-        $(".modal-body").shouldHave(text("Male"));
-        $(".modal-body").shouldHave(text("9650490205"));
-        $(".modal-body").shouldHave(text("20 Nov 1995"));
-        $(".modal-body").shouldHave(text("Mother Russia"));
+        $("tbody tr:nth-child(1) td:nth-child(2)").shouldHave(text("Askarov Chingiz"));
+        $("tbody tr:nth-child(2) td:nth-child(2)").shouldHave(text("chingiz-askarov@list.ru"));
+        $("tbody tr:nth-child(3) td:nth-child(2)").shouldHave(text("Male"));
+        $("tbody tr:nth-child(4) td:nth-child(2)").shouldHave(text("9650490205"));
+        $("tbody tr:nth-child(5) td:nth-child(2)").shouldHave(text("20 Novemder, 1995"));
+        $("tbody tr:nth-child(6) td:nth-child(2)").shouldHave(text("Mother Russia"));
+        $("tbody tr:nth-child(7) td:nth-child(2)").shouldHave(text("Sports"));
+        $("tbody tr:nth-child(8) td:nth-child(2)").shouldHave(text("flag.jpg"));
+        $("tbody tr:nth-child(9) td:nth-child(2)").shouldHave(text("Haryana Karnal"));
+
+
     }
 }
