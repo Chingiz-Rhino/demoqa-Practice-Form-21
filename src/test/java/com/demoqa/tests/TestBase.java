79b291b0cf3bef9b6ead40b1d298934343832b6c
Chingiz <chingiz-askarov@list.ru>
2023-08-29 21:57:41 +0300

    delete chromedriver settings from TestBase

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -9,8 +9,5 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1980x1200";
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ching\\OneDrive\\Рабочий стол\\Driver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        System.setProperty("selenide.browser", "Chrome");
-
     }
 }
