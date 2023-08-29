9a51a040fa6ab928765fbd14e73bcd18d17c6079
Chingiz <chingiz-askarov@list.ru>
2023-08-29 22:23:13 +0300

    added configuration.remote in TestBase

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -9,5 +9,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1980x1200";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
