4f1ce6a490aa062cb6607d00b70a9064a8296a0c
Chingiz <chingiz-askarov@list.ru>
2023-08-30 23:17:08 +0300

    added steps

--- src/test/java/com/demoqa/tests/PracticeFormTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -2,9 +2,12 @@ package com.demoqa.tests;
 
 
 import com.demoqa.pages.RegistrationPage;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 
 public class PracticeFormTests extends TestBase {
@@ -15,37 +18,41 @@ public class PracticeFormTests extends TestBase {
     @DisplayName("Testing Practice Form with hardcode")
     @Test
     void fillFormTest() {
-        registrationPage
-                .openWebSite()
-                .removeBanner()
-                .setFirstName("Chingiz")
-                .setLastName("Askarov")
-                .setUserEmail("chingiz-askarov@list.ru")
-                .setGender("Male")
-                .setUserPhoneNumber("9650490205")
-                .setBirthDate("20", "November", "1995")
-                .setUserSubjects("Computer Science")
-                .setHobbies("Sports")
-                .setUserPicture("flag.jpg")
-                .setUserAddress("Haryana Karnal")
-                .clickOnState()
-                .selectUserState("Haryana")
-                .clickOnCity()
-                .selectUserCity("Karnal")
-                .clickOnSubmit();
+        step("Open and fill the form", ()->{
+            registrationPage
+                    .openWebSite()
+                    .removeBanner()
+                    .setFirstName("Chingiz")
+                    .setLastName("Askarov")
+                    .setUserEmail("chingiz-askarov@list.ru")
+                    .setGender("Male")
+                    .setUserPhoneNumber("9650490205")
+                    .setBirthDate("20", "November", "1995")
+                    .setUserSubjects("Computer Science")
+                    .setHobbies("Sports")
+                    .setUserPicture("flag.jpg")
+                    .setUserAddress("Haryana Karnal")
+                    .clickOnState()
+                    .selectUserState("Haryana")
+                    .clickOnCity()
+                    .selectUserCity("Karnal")
+                    .clickOnSubmit();
+        });
+        step("Verify results", ()->{
+            registrationPage
+                    .checkTable()
+                    .checkTableText()
+                    .checkResult("Chingiz Askarov")
+                    .checkResult("chingiz-askarov@list.ru")
+                    .checkResult("Male")
+                    .checkResult("9650490205")
+                    .checkResult("20 November,1995")
+                    .checkResult("Computer Science")
+                    .checkResult("Sports")
+                    .checkResult("flag.jpg")
+                    .checkResult("Haryana Karnal");
+        });
 
-        registrationPage
-                .checkTable()
-                .checkTableText()
-                .checkResult("Chingiz Askarov")
-                .checkResult("chingiz-askarov@list.ru")
-                .checkResult("Male")
-                .checkResult("9650490205")
-                .checkResult("20 November,1995")
-                .checkResult("Computer Science")
-                .checkResult("Sports")
-                .checkResult("flag.jpg")
-                .checkResult("Haryana Karnal");
     }
 
 }
--- src/test/java/com/demoqa/tests/PracticeFormTestsWithUtils.java
+++ src/test/java/com/demoqa/tests/PracticeFormTestsWithUtils.java
@@ -5,48 +5,55 @@ import com.demoqa.pages.RegistrationPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 import static com.demoqa.utils.PropertiesOfData.*;
 
 
 public class PracticeFormTestsWithUtils extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
+
     @Tag("PracticeForm")
     @DisplayName("Testing Practice Form with java faker")
     @Test
     void fillFormTest() {
-        registrationPage
-                .openWebSite()
-                .removeBanner()
-                .setFirstName(firstNameFakeValue)
-                .setLastName(lastNameFakeValue)
-                .setUserEmail(emailFakeValue)
-                .setGender(genderFakeValue)
-                .setUserPhoneNumber(phoneNumberFakeValue)
-                .setBirthDate(dayOfBirthFakeValue, monthOfBirthFakeValue, yearOfBirthFakeValue)
-                .setUserSubjects(subjectFakeValue)
-                .setHobbies(hobbyFakeValue)
-                .setUserPicture(pictureFakeValue)
-                .setUserAddress(addressFakeValue)
-                .clickOnState()
-                .selectUserState(stateFakeValue)
-                .clickOnCity()
-                .selectUserCity(cityFakeValue)
-                .clickOnSubmit();
+        step("Open and fill form with java faker", () -> {
+            registrationPage
+                    .openWebSite()
+                    .removeBanner()
+                    .setFirstName(firstNameFakeValue)
+                    .setLastName(lastNameFakeValue)
+                    .setUserEmail(emailFakeValue)
+                    .setGender(genderFakeValue)
+                    .setUserPhoneNumber(phoneNumberFakeValue)
+                    .setBirthDate(dayOfBirthFakeValue, monthOfBirthFakeValue, yearOfBirthFakeValue)
+                    .setUserSubjects(subjectFakeValue)
+                    .setHobbies(hobbyFakeValue)
+                    .setUserPicture(pictureFakeValue)
+                    .setUserAddress(addressFakeValue)
+                    .clickOnState()
+                    .selectUserState(stateFakeValue)
+                    .clickOnCity()
+                    .selectUserCity(cityFakeValue)
+                    .clickOnSubmit();
+        });
+        step("Verify results", () -> {
+            registrationPage
+                    .checkTable()
+                    .checkTableText()
+                    .checkResult(firstNameFakeValue + " " + lastNameFakeValue)
+                    .checkResult(emailFakeValue)
+                    .checkResult(genderFakeValue)
+                    .checkResult(phoneNumberFakeValue)
+                    //"20 November,1995"
+                    .checkResult(dayOfBirthFakeValue + " " + monthOfBirthFakeValue + "," + yearOfBirthFakeValue)
+                    .checkResult(subjectFakeValue)
+                    .checkResult(hobbyFakeValue)
+                    .checkResult("flag.jpg")
+                    .checkResult(stateFakeValue + " " + cityFakeValue);
+        });
 
-        registrationPage
-                .checkTable()
-                .checkTableText()
-                .checkResult(firstNameFakeValue + " " + lastNameFakeValue)
-                .checkResult(emailFakeValue)
-                .checkResult(genderFakeValue)
-                .checkResult(phoneNumberFakeValue)
-                //"20 November,1995"
-                .checkResult(dayOfBirthFakeValue + " "+monthOfBirthFakeValue + ","+ yearOfBirthFakeValue)
-                .checkResult(subjectFakeValue)
-                .checkResult(hobbyFakeValue)
-                .checkResult("flag.jpg")
-                .checkResult(stateFakeValue+ " " +cityFakeValue);
     }
 
 }
